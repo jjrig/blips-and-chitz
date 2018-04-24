a72ed22af3583030da4da295ea8b13e71795dfc8
Jason Rigdon <jason.rigdon@gmail.com>
2018-04-23 22:18:08 -0400

    task: add some material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatButtonModule, MatCheckboxModule, MatCardModule, MatMenuModule, MatSidenavModule,
+  MatFormFieldModule, MatInputModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+const MaterialImports: any[] = [
+  MatButtonModule,
+  MatCheckboxModule,
+  MatCardModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatFormFieldModule,
+  MatInputModule,
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    BrowserAnimationsModule,
+  ].concat(MaterialImports),
   providers: [],
   bootstrap: [AppComponent]
 })
